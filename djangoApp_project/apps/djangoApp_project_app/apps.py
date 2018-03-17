398449767984579b83c9d8c955a402788bcd018b
Sun Davey <SunDavey@hotmail.com>
2018-03-17 16:09:10 -0700

    Basic routing setup on django python

---
+++ djangoApp_project/apps/djangoApp_project_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DjangoappProjectAppConfig(AppConfig):
+    name = 'djangoApp_project_app'
