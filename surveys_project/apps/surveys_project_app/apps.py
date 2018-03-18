5e3f42f554e126c64e8230c08fee0ae51f0e63d5
Sun Davey <SunDavey@hotmail.com>
2018-03-18 15:45:29 -0700

    Simple survey demonstration, passing info thru session

---
+++ surveys_project/apps/surveys_project_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SurveysProjectAppConfig(AppConfig):
+    name = 'surveys_project_app'
---
+++ surveys_project/apps/surveys_project_app/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'surveys_project_app/index.html')
+
+def process(request):
+    if 'count' in request.session:
+        request.session['count'] +=1
+    else:
+        request.session['count'] = 0
+    
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.session['favLan'] = request.POST['favLan']
+    request.session['comment'] = request.POST['comment']
+
+    return redirect('/result')
+
+def result(request):
+    return render(request, 'surveys_project_app/result.html')
