003268629562762f1c77f7f03284a9fdb04687fe
Sun Davey <SunDavey@hotmail.com>
2018-03-19 16:24:11 -0700

    revised the time zone

--- time_display_project/apps/time_display_project_app/views.py
+++ time_display_project/apps/time_display_project_app/views.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
 
 # Create your views here.
 def index(request):
     context = {
-        "date": strftime("%Y-%m-%d", gmtime()),
-        "time": strftime("%H:%M %p", gmtime())
+        "date": strftime("%Y-%m-%d", localtime()),
+        "time": strftime("%H:%M %p", localtime())
     }
     return render(request, 'time_display_project_app/index.html', context)
